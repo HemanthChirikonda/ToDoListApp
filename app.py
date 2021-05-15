c7a7b03ebd5cf9a9489038cc94f3f654cf0b3fba
hemanth_kumar <hemanth.kumar@cimpress.com>
2021-05-15 23:45:50 +0530

    intinal setup

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask.templating import render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']=r'sqlite:///C:\Users\HemanthKumar\Documents\github\cp-todo-flask\todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    text= db.Column(db.String(200))
+    complete= db.Column(db.Boolean)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
